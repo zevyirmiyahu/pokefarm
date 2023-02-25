8f6c1b6b24c0de19862ef33960f520f54c5cc032
Zev Yirmiyahu <zevyirmiyahu@gmail.com>
2023-02-25 12:55:38 -0800

    fixed typeo in comment

--- pokefarm-server/src/main/java/com/pokefarm/app/services/email/EmailService.java
+++ pokefarm-server/src/main/java/com/pokefarm/app/services/email/EmailService.java
@@ -3,7 +3,7 @@ package com.pokefarm.app.services.email;
 public interface EmailService {
 
 	/*
-	 * For sending an simple email
+	 * For sending a simple email
 	 */
 	public void sendEmail(final String userEmail, final String userName);
 }
